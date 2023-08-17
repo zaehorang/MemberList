1a796e74d0e02b00e3ac61f694903e574e7bee29
zaehorang <zaehorang2@naver.com>
2023-08-18 00:25:10 +0900

    Initial Commit

---
+++ MemberList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by zaehorang on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
